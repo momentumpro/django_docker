30cd45c3fc42fa74887e715b709d3a1942d7bf63
Adan Rosales <adan.rosales@apptegy.com>
2025-10-21 18:32:07 -0600

    fix api urls

--- lms/urls.py
+++ lms/urls.py
@@ -1,21 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from . import views
 
-# Create a router and register our viewsets with it
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'profiles', views.ProfileViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'courses', views.CourseViewSet)
-router.register(r'lessons', views.LessonViewSet)
-router.register(r'enrollments', views.EnrollmentViewSet)
-router.register(r'reviews', views.CourseReviewViewSet)
-router.register(r'lesson-progress', views.LessonProgressViewSet)
-
-# The API URLs are now determined automatically by the router
+# App URL patterns (site pages). The API router is mounted at project-level
 urlpatterns = [
-    path('/api/', include(router.urls)),
     path('', views.index, name='home'),
     path('registro/', views.registro, name='registro'),
     path('login/', views.iniciar_sesion, name='login'),
--- project/urls.py
+++ project/urls.py
@@ -1,25 +1,21 @@
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from lms import views as lms_views
+from rest_framework.routers import DefaultRouter
+
+# API router (register lms viewsets here so API lives under /api/lms/)
+api_router = DefaultRouter()
+api_router.register(r'users', lms_views.UserViewSet)
+api_router.register(r'profiles', lms_views.ProfileViewSet)
+api_router.register(r'categories', lms_views.CategoryViewSet)
+api_router.register(r'courses', lms_views.CourseViewSet)
+api_router.register(r'lessons', lms_views.LessonViewSet)
+api_router.register(r'enrollments', lms_views.EnrollmentViewSet)
+api_router.register(r'reviews', lms_views.CourseReviewViewSet)
+api_router.register(r'lesson-progress', lms_views.LessonProgressViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,6 +35,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include('lms.urls')),
     path('admin/', admin.site.urls),
+
+    path('api/lms/', include((api_router.urls, 'lms'), namespace='lms-api')),
     # DRF browsable API login
     path('api-auth/', include('rest_framework.urls')),
     # Swagger documentation
